6c12a2032f61bae062cbacdcca5654fd90958508
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-18 21:17:45 +0900

    초기 커밋.

---
+++ Lest_Report/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lest_Report
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
